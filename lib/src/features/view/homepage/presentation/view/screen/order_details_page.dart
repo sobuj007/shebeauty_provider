f07f302155ee040d78f969b4207a3a77bbccd79b
Md. Mehedi Hasan <68287045+Mehedihasan177@users.noreply.github.com>
2024-07-18 10:01:24 +0600

    all done
    Signed-off-by: Md. Mehedi Hasan <68287045+Mehedihasan177@users.noreply.github.com>

--- lib/src/features/view/homepage/presentation/view/screen/order_details_page.dart
+++ lib/src/features/view/homepage/presentation/view/screen/order_details_page.dart
@@ -303,7 +303,6 @@ class OrderDetailsPage extends StatelessWidget {
                   ),
                 ),
                 Container(
-                  height: MediaQuery.of(context).size.height,
                   width: MediaQuery.of(context).size.width,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.only(
@@ -344,7 +343,8 @@ class OrderDetailsPage extends StatelessWidget {
                       ],
                     ),
                   ),
-                )
+                ),
+                10.ph,
               ],
             ),
           ),
