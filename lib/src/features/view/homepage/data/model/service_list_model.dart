912882f92ae7144efd5a81c66a9c981764abbb86
MehediHasan <iotmehedi@gmail.com>
2024-09-03 17:20:08 +0600

    add service, store profile, my slots kaj baki ache

---
+++ lib/src/features/view/homepage/data/model/service_list_model.dart
@@ -0,0 +1,106 @@
+class ServiceModel {
+  String? message;
+  List<Data>? data;
+
+  ServiceModel({this.message, this.data});
+
+  ServiceModel.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+    if (json['data'] != null) {
+      data = <Data>[];
+      json['data'].forEach((v) {
+        data!.add(new Data.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['message'] = this.message;
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Data {
+  int? id;
+  String? agentId;
+  String? categoryId;
+  String? subcategoryId;
+  String? bodypartId;
+  String? cityId;
+  String? locationIds;
+  Null? slotId;
+  String? appointmentSlotIds;
+  String? name;
+  String? description;
+  String? image;
+  String? productPrice;
+  String? servicePrice;
+  String? gender;
+  String? createdAt;
+  String? updatedAt;
+
+  Data(
+      {this.id,
+      this.agentId,
+      this.categoryId,
+      this.subcategoryId,
+      this.bodypartId,
+      this.cityId,
+      this.locationIds,
+      this.slotId,
+      this.appointmentSlotIds,
+      this.name,
+      this.description,
+      this.image,
+      this.productPrice,
+      this.servicePrice,
+      this.gender,
+      this.createdAt,
+      this.updatedAt});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    agentId = json['agent_id'];
+    categoryId = json['category_id'];
+    subcategoryId = json['subcategory_id'];
+    bodypartId = json['bodypart_id'];
+    cityId = json['city_id'];
+    locationIds = json['location_ids'];
+    slotId = json['slot_id'];
+    appointmentSlotIds = json['appointment_slot_ids'];
+    name = json['name'];
+    description = json['description'];
+    image = json['image'];
+    productPrice = json['product_price'];
+    servicePrice = json['service_price'];
+    gender = json['gender'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['agent_id'] = this.agentId;
+    data['category_id'] = this.categoryId;
+    data['subcategory_id'] = this.subcategoryId;
+    data['bodypart_id'] = this.bodypartId;
+    data['city_id'] = this.cityId;
+    data['location_ids'] = this.locationIds;
+    data['slot_id'] = this.slotId;
+    data['appointment_slot_ids'] = this.appointmentSlotIds;
+    data['name'] = this.name;
+    data['description'] = this.description;
+    data['image'] = this.image;
+    data['product_price'] = this.productPrice;
+    data['service_price'] = this.servicePrice;
+    data['gender'] = this.gender;
+    data['created_at'] = this.createdAt;
+    data['updated_at'] = this.updatedAt;
+    return data;
+  }
+}
---
+++ lib/src/features/widgets/custom_dropdown_button/custom_dropdown_button.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+import 'package:shebeauty_provider/src/core/model/dropdown_model.dart';
+import 'package:shebeauty_provider/src/features/widgets/custom_text/custom_text.dart';
+
+class CustomDropDown extends StatelessWidget {
+  final String? fieldTitle, hint;
+  final DropdownModel? selectedItem;
+  final List<DropdownModel> spinnerItemList;
+  final Color? dropdownColor;
+  final Color? titleColor, valueTextColor;
+  final bool? isEnable;
+  final double? manuMaxHeight, titleFontSize;
+  final void Function(DropdownModel) onChanged;
+
+  const CustomDropDown(
+      {Key? key,
+      this.fieldTitle,
+      required this.spinnerItemList,
+      required this.onChanged,
+      this.selectedItem,
+      this.dropdownColor,
+      this.hint,
+      this.titleColor,
+      this.manuMaxHeight,
+      this.isEnable,
+      this.titleFontSize,
+      this.valueTextColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (fieldTitle?.isNotEmpty ?? false)
+          FieldTitleText(
+            text: fieldTitle!,
+            color: titleColor,
+            fontSize: titleFontSize,
+          ),
+        if (fieldTitle?.isNotEmpty ?? false)
+          const SizedBox(
+            height: 3,
+          ),
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                height: 50,
+                decoration: BoxDecoration(
+                  // border: Border.all(color: Colors.grey, width: 1),
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.white,
+                  border: Border.all(
+                    width: 1,
+                    color: Colors.grey.withOpacity(0.4),
+                  ),
+                ),
+                child: DropdownButton<DropdownModel>(
+                  dropdownColor: dropdownColor ?? Colors.white,
+                  hint: Text(
+                    "$hint",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                  value: selectedItem,
+
+                  icon: const Icon(Icons.arrow_drop_down),
+                  underline: const SizedBox(),
+                  isExpanded: true,
+                  // Hide default dropdown icon
+                  elevation: 16,
+                  menuMaxHeight: manuMaxHeight,
+                  style: const TextStyle(color: Colors.red, fontSize: 18),
+                  onChanged: isEnable == false
+                      ? null
+                      : (newValue) {
+                          onChanged(newValue!);
+                        },
+                  items: spinnerItemList.map<DropdownMenuItem<DropdownModel>>(
+                      (DropdownModel value) {
+                    return DropdownMenuItem<DropdownModel>(
+                      key: UniqueKey(), // Set a unique key here
+                      value: value,
+                      child: CustomText(
+                        text: value.name,
+                        textColor: valueTextColor ?? Colors.black,
+                        textAlign: TextAlign.start,
+                        fontSize: 18,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class CustomDropDownForThreeData extends StatelessWidget {
+  final String? fieldTitle, hint;
+  final DropdownModelForThreeValue? selectedItem;
+  final List<DropdownModelForThreeValue> spinnerItemList;
+  final Color? dropdownColor;
+  final Color? titleColor, valueTextColor;
+  final bool? isEnable;
+  final double? manuMaxHeight, titleFontSize;
+  final void Function(DropdownModelForThreeValue) onChanged;
+
+  const CustomDropDownForThreeData(
+      {Key? key,
+      this.fieldTitle,
+      required this.spinnerItemList,
+      required this.onChanged,
+      this.selectedItem,
+      this.dropdownColor,
+      this.hint,
+      this.titleColor,
+      this.manuMaxHeight,
+      this.isEnable,
+      this.titleFontSize,
+      this.valueTextColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        fieldTitle != null
+            ? FieldTitleText(
+                text: fieldTitle!,
+                color: titleColor,
+                fontSize: titleFontSize,
+              )
+            : SizedBox(),
+        fieldTitle != null
+            ? const SizedBox(
+                height: 3,
+              )
+            : SizedBox(),
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                height: 50,
+                decoration: BoxDecoration(
+                  // border: Border.all(color: Colors.grey, width: 1),
+                  borderRadius: BorderRadius.circular(5),
+                  // color: Colors.grey.withOpacity(0.1),
+                  border: Border.all(
+                    width: 0.6,
+                    color: Colors.grey.withOpacity(0.4),
+                  ),
+                ),
+                child: DropdownButton<DropdownModelForThreeValue>(
+                  dropdownColor: dropdownColor ?? Colors.white,
+                  hint: Text(
+                    "$hint",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                  value: selectedItem,
+
+                  icon: const Icon(Icons.arrow_drop_down),
+                  underline: const SizedBox(),
+                  isExpanded: true,
+                  // Hide default dropdown icon
+                  elevation: 16,
+                  menuMaxHeight: manuMaxHeight,
+                  style: const TextStyle(color: Colors.red, fontSize: 18),
+                  onChanged: isEnable == false
+                      ? null
+                      : (newValue) {
+                          onChanged(newValue!);
+                        },
+                  items: spinnerItemList
+                      .map<DropdownMenuItem<DropdownModelForThreeValue>>(
+                          (DropdownModelForThreeValue value) {
+                    return DropdownMenuItem<DropdownModelForThreeValue>(
+                      key: UniqueKey(), // Set a unique key here
+                      value: value,
+                      child: CustomText(
+                        text: value.name,
+                        textColor: valueTextColor ?? Colors.black,
+                        textAlign: TextAlign.start,
+                        fontSize: 18,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
